8b3d37eeae03f8232211fa221db4136e9d98b7cd
UnwiseGiraffeX86 <stefanut.geala@gmail.com>
2024-06-14 20:36:27 +0100

    .

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Form submitted!');
+    // Here you can add functionality to send the form data to your server or email.
+});
